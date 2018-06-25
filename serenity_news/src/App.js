b6d7c09c04cb650ed0938e8a548c1ab7c5aa8c4d
DustinC1 <dbcenatiempo@gmail.com>
2018-06-25 01:22:00 -0700

    Working project!

--- serenity_news/src/App.js
+++ serenity_news/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PropTypes from 'prop-types';
 
 const propTypes = {
 	title: PropTypes.string,
 	url: PropTypes.string,
-	urlToImage: PropTypes.string
+	urlToImage: PropTypes.string,
 };
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			news: {}
+			news: {},
+			inputText: ''
 		};
 	}
 	
@@ -26,15 +26,59 @@ class App extends Component {
 							news: data
 						};
 					});
+					console.log(this.state.news.news);
+			});
+	}
+	
+	onInputChange = (e) => {
+			const inputText = e.target.value;
+			this.setState((prevState, props) => {
+					return {
+							inputText: inputText
+					};
+			});
+	}
+
+	filterArticles = () => {
+			this.setState((prevState, props) => {
+					//const news = prevState.news;
+					let newsCopy = Object.assign({}, this.state.news); //creating copy of news object in state
+					for (var article in newsCopy.news.articles) {
+						if (newsCopy.news.articles[article].description.includes(this.state.inputText)) {
+							newsCopy.news.articles[article].title = '';
+							newsCopy.news.articles[article].description = 'SERENITY NOW';
+							newsCopy.news.articles[article].urlToImage = 'Serenity.jpg';
+						}
+					}
+					return {
+							news: newsCopy,
+							inputText: ''
+					};
 			});
 	}
 	
   render() {
     return (
-       <div className="App">
-				<h2>Serenity News</h2>
-				{JSON.stringify(this.state.news)}
-      </div> 
+      <div className="App">
+				<p className="Title-text">Serenity News</p>
+				<br></br><br></br>
+				<form>
+					<span className="Input-desc">Noise to block: </span>
+					<input onChange={this.onInputChange} value={this.state.inputText}></input>
+					<button disabled={this.state.inputText === ''} onClick={this.filterArticles}>Apply</button>
+					<br></br><br></br><br></br>
+				</form>
+				{this.state.news.news ? this.state.news.news.articles.map((article, idx) => 
+					<div className="grid-card">
+						<img className="Article-img" src={article.urlToImage} ></img>
+						<div className="grid-card-right">
+							<p className="Article-title"><a href={article.url}>{article.title}</a></p>
+							<p className="Article-desc">{article.description}</p>
+						</div>
+					</div> ) 
+					: <p>No articles, be patient.</p>
+				}
+      </div>
     );
   }
 }
